d4a6e951c9c700e10f86ed819329c1393ee13f30
hogumachu <castle_jun@daum.net>
2021-11-29 20:44:03 +0900

    init

---
+++ AWord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AWord
+//
+//  Created by 홍성준 on 2021/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
