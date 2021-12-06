ac2aa364f274b868abfe942275c83a36ec3c2213
hogumachu <castle_jun@daum.net>
2021-12-06 14:25:25 +0900

    Prevent LandScape

--- AWord/SupportingFiles/AppDelegate.swift
+++ AWord/SupportingFiles/AppDelegate.swift
@@ -10,9 +10,12 @@ import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    // MARK: - Prevent LandScape
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return [.portrait]
+    }
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
