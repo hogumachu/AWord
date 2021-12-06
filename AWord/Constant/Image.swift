a3cfcdb1805be3674cbb2458a5deb2952c70dd35
hogumachu <castle_jun@daum.net>
2021-12-06 20:16:09 +0900

    Add new images

--- AWord/Constant/Image.swift
+++ AWord/Constant/Image.swift
@@ -4,15 +4,20 @@ public let _plus = UIImage(systemName: "plus")?.withTintColor(_titleColor, rende
 public let _plusCircle = UIImage(systemName: "plus.circle")?.withTintColor(_titleColor, renderingMode: .alwaysOriginal)
 public let _plusCircleFill = UIImage(systemName: "plus.circle.fill")?.withTintColor(_titleColor, renderingMode: .alwaysOriginal)
 
-public let _xmark = UIImage(systemName: "xmark")?.withTintColor(_titleColor, renderingMode: .alwaysOriginal)
 
 public let _xmarkCircle = UIImage(systemName: "xmark.circle")?.withTintColor(_titleColor, renderingMode: .alwaysOriginal)
 public let _xmarkCircleFill =  UIImage(systemName: "xmark.circle.fill")?.withTintColor(_titleColor, renderingMode: .alwaysOriginal)
 
 public let _arrowUturnBackward = UIImage(systemName: "arrow.uturn.backward")?.withTintColor(_titleColor, renderingMode: .alwaysOriginal)
 
+public let _checkmark = UIImage(systemName: "checkmark")?.withTintColor(.systemGreen, renderingMode: .alwaysOriginal)
 public let _checkmarkCircle = UIImage(systemName: "checkmark.circle")?.withTintColor(.systemGreen, renderingMode: .alwaysOriginal)
 public let _checkmarkCircleFill = UIImage(systemName: "checkmark.circle.fill")?.withTintColor(.systemGreen, renderingMode: .alwaysOriginal)
 
+public let _xmark = UIImage(systemName: "xmark")?.withTintColor(_titleColor, renderingMode: .alwaysOriginal)
+public let _xmarkRed = UIImage(systemName: "xmark")?.withTintColor(.systemRed, renderingMode: .alwaysOriginal)
+
 public let _xmarkCircleRed = UIImage(systemName: "xmark.circle")?.withTintColor(.systemRed, renderingMode: .alwaysOriginal)
 public let _xmarkCircleFillRed =  UIImage(systemName: "xmark.circle.fill")?.withTintColor(.systemRed, renderingMode: .alwaysOriginal)
+
+public let _speaker = UIImage(systemName: "speaker.wave.2")?.withTintColor(_titleColor, renderingMode: .alwaysOriginal)
